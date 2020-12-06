41b09cc109ded00a29dd852081b783553a59fe92
AsthaTibrewal <75210550+AsthaTibrewal@users.noreply.github.com>
2020-12-06 19:51:22 +0530

    css-updated

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var btnTranslate=document.querySelector("#btn-translate");
+var textInput=document.querySelector("#txt-input");
+console.log(textInput)
 btnTranslate.addEventListener("click",function clickEventListenerHandler() {
-
     console.log("clicked ")
+    console.log("input",textInput.value)
+
 }) 
